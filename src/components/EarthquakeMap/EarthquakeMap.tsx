dd21f95461d58194aedbc50615e998a3e2298a19
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-10-06 01:00:41 -0500

    [add][fix] adding seismic details

--- src/components/EarthquakeMap/EarthquakeMap.tsx
+++ src/components/EarthquakeMap/EarthquakeMap.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle, Tooltip } from 'react-leaflet';
-// import * as d3 from 'd3';
 import './EarthquakeMap.css';
 
 interface Earthquake {
